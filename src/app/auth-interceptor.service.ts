c89cf14836cea4729f59e11b09d2708c01e0cb45
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-08-09 11:04:16 +0530

    9 aug 2023
    1104am

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,17 +3,17 @@ import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req : HttpRequest<any>,next : HttpHandler){
-        console.log('Request is on the way');
-        console.log(req.url);
+        //console.log('Request is on the way');
+        //console.log(req.url);
         const modifiedRequest = req.clone({
             headers : req.headers.append('Auth','xyz')
         });
         //return next.handle(req);
         return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event);
+            //console.log(event);
             if(event.type === HttpEventType.Response){
-                console.log('Response arrived');
-                console.log(event.body);
+                //console.log('Response arrived');
+                //console.log(event.body);
             }
         }));
     }
